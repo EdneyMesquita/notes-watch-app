4054173d163714a3fda2d18630a712e7c71d0419
Edney Mesquita <edneyphp@outlook.com>
2022-05-05 23:06:38 -0300

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Edney Mesquita on 05/05/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
